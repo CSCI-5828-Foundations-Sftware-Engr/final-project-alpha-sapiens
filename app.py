ecbff908b9559d90ee175ee914ce0a81899ea848
Code-Malone <54590458+Code-Malone@users.noreply.github.com>
2023-03-19 23:15:35 -0500

    homework 5

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+	if request.args.get('name') != None:
+		return render_template('name.html', name=request.args.get('name'))
+	return render_template('form.html')
