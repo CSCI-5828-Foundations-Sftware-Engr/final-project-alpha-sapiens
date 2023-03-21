b0085edd3a786e0bcda15058a5af0eed19431177
sko-code-malone <mafo8956@colorado.edu>
2023-03-21 15:24:12 -0600

    move directories

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def form():
-	if request.args.get('name') != None:
-		return render_template('name.html', name=request.args.get('name'))
-	return render_template('form.html')
